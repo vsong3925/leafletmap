5a9ce552421883b6b9a2f5b3a8ed8aa5d70c8bf0
Vera Song <verasong@Veras-Air.fios-router.home>
2021-04-13 15:44:03 -0400

    update files

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -1,3 +1,5 @@
+const API_KEY = "pk.eyJ1Ijoia3dhbWU2IiwiYSI6ImNra2VscHl5eDAxMTMybnFueDZ0d2dxN3cifQ.-Ju8fppjeiM4KteEOq_RkQ";
+
 
 // **********************************
 // ********** baseMap() *************
